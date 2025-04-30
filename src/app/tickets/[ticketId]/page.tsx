cba8f558e23bbd573beebab80197fdf449927471
Emmanuel Egomson <fegomson@gmail.com>
2025-04-29 22:03:05 -0500

    chore: replace cards with shadcn card

--- src/app/tickets/[ticketId]/page.tsx
+++ src/app/tickets/[ticketId]/page.tsx
@@ -1,31 +1,10 @@
 import { initialTickets } from "@/data";
-import { FaRegCheckCircle } from "react-icons/fa";
-import { BsHourglassSplit } from "react-icons/bs";
-import { LuFileJson2 } from "react-icons/lu";
 
 interface TicketProps {
   params: {
     ticketId: Promise<string>;
   };
 }
-
-const TICKET_ICONS = {
-  OPEN: (
-    <div className="text-blue-500 flex gap-1 items-center">
-      <LuFileJson2 /> <span className="text-xs">OPEN</span>
-    </div>
-  ),
-  CLOSED: (
-    <div className="text-green-500 flex gap-1 items-center">
-      <FaRegCheckCircle /> <span className="text-xs">CLOSED</span>
-    </div>
-  ),
-  "IN PROGRESS": (
-    <div className="text-yellow-500 flex gap-1 items-center">
-      <BsHourglassSplit /> <span className="text-xs">IN PROGRESS</span>
-    </div>
-  ),
-};
 
 const Ticket = async ({ params }: TicketProps) => {
   const ticketId = await params.ticketId;
@@ -41,7 +20,6 @@ const Ticket = async ({ params }: TicketProps) => {
 
   return (
     <div>
-      <div>{TICKET_ICONS[ticket.status]}</div>
       <h2 className="text-lg">{ticket?.title}</h2>
       <p className="text-sm">{ticket?.content}</p>
     </div>
--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,8 +1,34 @@
 import { initialTickets } from "@/data";
 import { ticketsPath } from "@/paths";
 import Link from "next/link";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { LuFileJson2 } from "react-icons/lu";
+import { FaRegCheckCircle } from "react-icons/fa";
+import { BsHourglassSplit } from "react-icons/bs";
 
-import clsx from "clsx";
+const TICKET_ICONS = {
+  OPEN: (
+    <div className="text-blue-500">
+      <LuFileJson2 title="open" />
+    </div>
+  ),
+  CLOSED: (
+    <div className="text-green-500">
+      <FaRegCheckCircle title="closed" />
+    </div>
+  ),
+  "IN PROGRESS": (
+    <div className="text-yellow-500">
+      <BsHourglassSplit title="in progress" />
+    </div>
+  ),
+};
 
 const Tickets = () => {
   return (
@@ -15,22 +41,26 @@ const Tickets = () => {
       </div>
       <div className="flex flex-1 flex-col items-center gap-y-4 animate-fade-in-from-top">
         {initialTickets.map((ticket) => (
-          <div key={ticket.id} className="border p-4 w-full max-w-[400px]">
-            <h2 className="text-lg">{ticket.title}</h2>
-            <p
-              className={clsx("text-sm text-slate-400 truncate", {
-                "line-through": ticket.status === "CLOSED",
-              })}
-            >
-              {ticket.content}
-            </p>
-            <Link
-              href={`${ticketsPath}/${ticket.id}`}
-              className="text-sm font-medium text-muted-foreground hover:text-primary cursor-pointer hover:underline"
-            >
-              View
-            </Link>
-          </div>
+          <Card key={ticket.id} className="w-full max-w-[400px]">
+            <CardHeader>
+              <CardTitle className="text-lg">
+                <span className="flex items-center gap-x-2">
+                  {TICKET_ICONS[ticket.status]} {ticket.title}
+                </span>
+              </CardTitle>
+            </CardHeader>
+            <CardContent>
+              <span className="line-clamp-3">{ticket.content}</span>
+            </CardContent>
+            <CardFooter>
+              <Link
+                href={`${ticketsPath}/${ticket.id}`}
+                className="text-sm font-medium text-muted-foreground hover:text-primary cursor-pointer hover:underline"
+              >
+                View
+              </Link>
+            </CardFooter>
+          </Card>
         ))}
       </div>
     </div>
