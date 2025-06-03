4cddd136132911df030084399ca688e00bffe830
Emmanuel Egomson <fegomson@gmail.com>
2025-06-03 11:19:48 -0500

    fix: error fetching tickets

--- src/features/ticket/components/ticket-list.tsx
+++ src/features/ticket/components/ticket-list.tsx
@@ -1,8 +1,13 @@
 import TicketItem from "./ticket-item";
 import { getTickets } from "../queries/get-tickets";
+import { notFound } from "next/navigation";
 
 const TicketList = async () => {
   const tickets = await getTickets();
+
+  if (!tickets || tickets.length === 0) {
+    return notFound();
+  }
 
   return (
     <div className="flex flex-1 flex-col items-center gap-y-4 animate-fade-in-from-top">
