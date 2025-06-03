b5bac4d0877abaaa6a6452851cb82320b0af0cf1
Emmanuel Egomson <fegomson@gmail.com>
2025-06-03 12:11:22 -0500

    chore: redirect after deleting ticket

--- src/features/ticket/actions/delete-ticket.ts
+++ src/features/ticket/actions/delete-ticket.ts
@@ -1,9 +1,12 @@
 "use server";
 
 import { prisma } from "@/lib/prisma";
+import { redirect } from "next/navigation";
 
 export const deleteTicket = async (id: string) => {
   await prisma.ticket.delete({
     where: { id },
   });
+
+  redirect("/tickets");
 };
